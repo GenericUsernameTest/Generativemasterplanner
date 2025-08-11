308b88f4649cd0e755eac1e88fed2e951a23bd51
GenericUsernameTest <joe.higson@asembl.io>
2025-08-11 14:49:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-import { generateMasterplan } from './homes.js';
+import { generateMasterplan, getLongestEdgeAngle } from './homes.js';
+
+function $(id) { return document.getElementById(id); }
+
+function updateGeoJSONSource(id, data) {
+  if (map.getSource(id)) {
+    map.getSource(id).setData(data);
+  } else {
+    map.addSource(id, { type: 'geojson', data });
+  }
+}
 
 function doFill() {
   if (!siteBoundary) { alert('Draw the site boundary first.'); return; }
@@ -22,3 +32,6 @@ function doFill() {
     Roads: ${(turf.area(roads)/10000).toFixed(2)} ha<br>
     Density: ${(homes.features.length / (turf.area(siteBoundary) / 10000)).toFixed(1)} homes/ha</p>`);
 }
+
+// Hook up your buttons here (drawSite, clearAll, fillHomes -> doFill)
+document.getElementById('fillHomes').addEventListener('click', doFill);
